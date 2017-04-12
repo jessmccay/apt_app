21795586f3349fccd196ecbf3fdd9252f2cf73ff
jessmccay <jessmccay@gmail.com>
2017-04-12 11:04:10 -0700

    creating an aparment feature test

--- spec/features/cool_features_spec.rb
+++ spec/features/cool_features_spec.rb
@@ -16,6 +16,25 @@ RSpec.feature "CoolFeatures", type: :feature do
         expect(page).to have_content "Phone number"
         expect(page).to have_content "Hours"
       end
+    end #close steps
+  end # close context
+  context "creating a new apartment listing" do
+    Steps "to create a new apartment listing" do
+      Given "I am on the new apartment page" do
+        visit "/apartments/new"
+      end
+      And "I can fill out the new apartment form" do
+        fill_in "Street1", with: "Garnet Ave"
+        fill_in "Street2", with: "Olney St"
+        fill_in "City", with: "SD"
+        fill_in "State", with: "CA"
+        fill_in "Country", with: "USA"
+        fill_in "Name", with: "Gretta's apartments"
+        fill_in "Phone number", with: "555-5555"
+        fill_in "Hours", with: "9am-5pm"
+        click_on "Create Apartment"
+        expect(page).to have_content "Apartment was successfully created."
+      end
     end
-  end
+  end  # close context
 end
